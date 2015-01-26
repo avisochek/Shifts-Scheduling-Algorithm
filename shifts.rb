69f9c937f3fb6df638650500214ab7d2097db5e3
Allan Visochek <avisochek3@gmail.com>
2015-01-25 21:53:34 -0500

    Adding shifts .rb

---
+++ shifts.rb
@@ -0,0 +1,25 @@
+def score(request_id,high-low) ## assign a score to each of the truncations for the given request
+			       ## return truncation id of the highest or lowest scoring one. 
+
+	return truncation_id, score
+
+end
+
+
+shifts = 
+
+max_iter = 
+
+N = 
+while count < max_iter
+	@order = Array.new(10) {|i| i+1}
+	@order = @order.shuffle()
+	for i in @order
+		if roster[i][''] then
+			roster[i]['active']
+			requests[i]
+		else
+			roster[i]['inactive']
+	end
+end
+

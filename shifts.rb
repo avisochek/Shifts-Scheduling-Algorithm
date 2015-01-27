b334be087d897404e046e8c8c9a6edbe31b9a215
avisochek <avisochek3@gmail.com>
2015-01-27 16:16:37 -0500

    more changes...

--- shifts.rb
+++ shifts.rb
@@ -1,5 +1,5 @@
-def get_cost(request_id,high-low) ## assign a score to each of the truncations for the given request
-			       ## return truncation id of the highest or lowest scoring one. 
+def get_cost(request_id,block_id,add/drop,) ## assign a score to each of the truncations for the given request
+			       ## return truncation id of the highest or lowest scoring one out of those which add or drop the given employee from the current block. 
 
 	return truncation_id, score
 
@@ -16,21 +16,29 @@ while count < max_iter
 	@order = @order.shuffle()
 
 	for i in @order ### iterate through the 
-		if roster[i]['staffed'] then ### drop the request from the roster with the 
+		if roster[i]['scheduled_'] then ### drop the request from the roster with the 
 			min_cost_drop = 0
 			for q in roster[i]['active_requests']
-				truncation_id, cost = get_cost(q)
+				truncation_id, cost = get_cost(q,i,0)
 				if cost < min_cost_drop
-					drop = q
+					drop = [request_id,truncation_id]
 				end
 			end
 
-			roster[i]['active_requests'].remove(q)
-			roster[i]['inactive_requests'].append(q)
 			requests[q]['shift'] = truncation_id
+			update_roster(drop)
 
-		else if 
-			roster[i]['inactive']
+		else if roster[i]['staffed'] then ### add the request from the roster with the 
+			min_cost_add = 
+			for q in roster[i]['active_requests']
+				truncation_id, cost = get_cost(q,i,1)
+				if cost < min_cost_drop
+					add = [request_id,truncation_id]
+				end
+			end
+
+			requests[q]['shift'] = truncation_id
+			update_roster(add)
 	end
 end
 

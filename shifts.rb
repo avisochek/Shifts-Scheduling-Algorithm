dbae72611174d67eb23096e048ba5abce4e83062
Allan Visochek <avisochek3@gmail.com>
2015-01-27 13:55:22 -0500

    made some changes...

--- shifts.rb
+++ shifts.rb
@@ -1,4 +1,4 @@
-def score(request_id,high-low) ## assign a score to each of the truncations for the given request
+def get_cost(request_id,high-low) ## assign a score to each of the truncations for the given request
 			       ## return truncation id of the highest or lowest scoring one. 
 
 	return truncation_id, score
@@ -14,11 +14,22 @@ N =
 while count < max_iter
 	@order = Array.new(10) {|i| i+1}
 	@order = @order.shuffle()
-	for i in @order
-		if roster[i][''] then
-			roster[i]['active']
-			requests[i]
-		else
+
+	for i in @order ### iterate through the 
+		if roster[i]['staffed'] then ### drop the request from the roster with the 
+			min_cost_drop = 0
+			for q in roster[i]['active_requests']
+				truncation_id, cost = get_cost(q)
+				if cost < min_cost_drop
+					drop = q
+				end
+			end
+
+			roster[i]['active_requests'].remove(q)
+			roster[i]['inactive_requests'].append(q)
+			requests[q]['shift'] = truncation_id
+
+		else if 
 			roster[i]['inactive']
 	end
 end
